2803be8c70b3e342ebc851ab58149d974a62587c
Ilya Voronin <ivoronin@octalwave.com>
2026-06-22 11:02:08 +0300

    Fix shellcheck findings in cloud-init.sh

--- cloud-init.sh
+++ cloud-init.sh
@@ -11,6 +11,7 @@ get_data() {
 }
 
 setup_ssh_keys() {
+	# shellcheck disable=SC2174
 	mkdir -p -m 700 "${SSH_KEY_USER_HOME}/.ssh"
 	chown "${SSH_KEY_USER}:${SSH_KEY_USER_GID}" "${SSH_KEY_USER_HOME}/.ssh"
 
@@ -21,14 +22,14 @@ setup_ssh_keys() {
 
 	mv "${SSH_AUTHORIZED_KEYS}" "${SSH_AUTHORIZED_KEYS}.bak"
 	awk '/^# cloud-init/{c=2} !(c&&c--)' > "${SSH_AUTHORIZED_KEYS}" < "${SSH_AUTHORIZED_KEYS}.bak"
-	echo "# cloud-init\n${SSH_KEY}" >> "${SSH_AUTHORIZED_KEYS}"
+	printf '# cloud-init\n%s\n' "${SSH_KEY}" >> "${SSH_AUTHORIZED_KEYS}"
 	rm "${SSH_AUTHORIZED_KEYS}.bak"
 }
 
 setup_local_hostname() {
 	echo "${LOCAL_HOSTNAME}" > /etc/myname
 	hostname "${LOCAL_HOSTNAME}"
-	LOCAL_SHORTNAME="$(echo ${LOCAL_HOSTNAME} | cut -d. -f1)"
+	LOCAL_SHORTNAME="${LOCAL_HOSTNAME%%.*}"
 	LOCAL_NAMES="localhost ${LOCAL_SHORTNAME}"
 	if [ "${LOCAL_SHORTNAME}" != "${LOCAL_HOSTNAME}" ]; then
 		LOCAL_NAMES="${LOCAL_NAMES} ${LOCAL_HOSTNAME}"
@@ -37,20 +38,16 @@ setup_local_hostname() {
 		-e "s/^::1[[:space:]].*/::1 ${LOCAL_NAMES}/" /etc/hosts
 }
 
-SSH_KEY="$(get_data meta-data/public-keys/0/openssh-key)"
-if [[ $? -eq 0 && -n "${SSH_KEY}" ]]; then
+if SSH_KEY="$(get_data meta-data/public-keys/0/openssh-key)" && [ -n "${SSH_KEY}" ]; then
 	SSH_AUTHORIZED_KEYS="${SSH_KEY_USER_HOME}/.ssh/authorized_keys"
 
-	grep -q "^${SSH_KEY}$" "${SSH_AUTHORIZED_KEYS}" 2> /dev/null
-	if [ $? -ne 0 ]; then
+	if ! grep -q "^${SSH_KEY}$" "${SSH_AUTHORIZED_KEYS}" 2> /dev/null; then
 		setup_ssh_keys
 	fi
 fi
 
-LOCAL_HOSTNAME="$(get_data meta-data/local-hostname)"
-if [[ $? -eq 0 && -n "${LOCAL_HOSTNAME}" ]]; then
-	grep -q "^${LOCAL_HOSTNAME}$" /etc/myname
-	if [ $? -ne 0 ]; then
+if LOCAL_HOSTNAME="$(get_data meta-data/local-hostname)" && [ -n "${LOCAL_HOSTNAME}" ]; then
+	if ! grep -q "^${LOCAL_HOSTNAME}$" /etc/myname; then
 		setup_local_hostname
 	fi
 fi
